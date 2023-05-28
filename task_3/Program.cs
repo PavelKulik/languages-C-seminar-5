7e5d6ff551b156cf8801aa6c91afce74ea759c2e
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-28 22:48:30 +0300

    task_3 add

---
+++ task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿internal partial class Program_3
+{
+    private static void Main(string[] args)
+    {
+        double[] GetArray(int size)
+        {
+            double[] array = new double[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = new Random().NextDouble() * 10;
+            }
+
+            return array;
+        }
+
+        void PrintArray(double[] array)
+        {
+            Console.Write($"[{String.Join(", ", array)}]");
+        }
+
+        double DifferenceMaxMin(double[] array)
+        {
+            double max = 0;
+            double min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+
+                if (min > array[i])
+                    min = array[i];
+            }
+
+            return max - min;
+        }
+
+        double[] array = GetArray(5);
+        PrintArray(array);
+        Console.WriteLine($" -> {DifferenceMaxMin(array)}");
+    }
+}
