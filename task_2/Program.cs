14d9ba0d42b44a4bfe9e04433b296325d1c43349
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-28 22:32:28 +0300

    task_2 add

---
+++ task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal partial class Program_2
+{
+    private static void Main(string[] args)
+    {
+        int[] GetArray(int size, int minValue, int maxValue)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = new Random().Next(minValue, maxValue);
+            }
+
+            return array;
+        }
+
+        void PrintArray(int[] array)
+        {
+            Console.Write($"[{String.Join(", ", array)}]");
+        }
+
+        int SumElement(int[] array)
+        {
+            int sum = 0;
+            for (int i = 1; i < array.Length; i = i + 2)
+                sum += array[i];
+            return sum;
+        }
+
+        int [] array = GetArray(5, 1, 30);
+        PrintArray(array);
+        Console.WriteLine($" -> {SumElement(array)}");
+    }
+}
